5daec9103da6137b5d20d078d5321cddadd74e44
chaoli <lichao01.ah@chinatelecom.cn>
2025-09-10 07:49:36 +0800

    增加测试用例

---
+++ src/main/java/com/chinatelecom/udp/component/shardingsphere/PostgresViewRewriter.java
@@ -0,0 +1,326 @@
+package com.chinatelecom.udp.component.shardingsphere;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNodeImpl;
+import org.apache.shardingsphere.infra.database.core.type.DatabaseType;
+import org.apache.shardingsphere.infra.spi.type.typed.TypedSPILoader;
+import org.apache.shardingsphere.proxy.backend.session.ConnectionSession;
+import org.apache.shardingsphere.sql.parser.api.CacheOption;
+import org.apache.shardingsphere.sql.parser.api.SQLParserEngine;
+
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.AliasContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.AssignmentContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.AssignmentValuesContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.ColumnRefContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.DeleteContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.ExprContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.FieldsContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.InsertContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.InsertSelectClauseContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.InsertValuesClauseContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.ProjectionsContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.SelectContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.SetAssignmentsClauseContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.SimpleExprContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.SubqueryContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.TableFactorContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.TableNameContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.UpdateContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.WhereClauseContext;
+import org.apache.shardingsphere.sql.parser.core.ParseASTNode;
+import org.apache.shardingsphere.sql.parser.exception.SQLParsingException;
+
+public class PostgresViewRewriter extends ViewRewriter{
+
+	private static final Logger LOGGER = Logger.getLogger(PostgresViewRewriter.class.getName());
+
+	private static PostgresViewRewriter instance;
+
+	public static PostgresViewRewriter getInstance(){
+		if(instance == null){
+			synchronized (PostgresViewRewriter.class) {
+				if (instance == null) {
+					instance=new PostgresViewRewriter(
+						new SQLParserEngine(TypedSPILoader.getService(DatabaseType.class, "PostgreSQL"), new CacheOption(2000, 65535L))
+					);
+				}
+			}
+		}
+		return instance;
+	}
+
+	
+	public PostgresViewRewriter(){
+
+	}
+
+	public PostgresViewRewriter(SQLParserEngine parserEngine){
+		super(parserEngine,"PostgreSQL");
+	}
+	
+	public String rewriteSql(String userName,String sql) {
+		ParseASTNode parseASTNode = parserEngine.parse(sql, false);
+		ParseTree rootNode = parseASTNode.getRootNode();
+		if (rootNode instanceof SelectContext){
+			rewriteTableFactor(userName,rootNode);
+		} else if (rootNode instanceof InsertContext){
+			rewriteInsertContext(userName,rootNode);
+		} else if (rootNode instanceof UpdateContext){
+			rewriteUpdateContext(userName,rootNode);
+		} else if (rootNode instanceof DeleteContext){
+			rewriteDeleteContext(userName,rootNode);
+		} else{
+			return sql;
+		}
+		printStructure(rootNode,0);
+		return printSql(rootNode);
+	}
+
+	public void rewriteInsertContext(String userName,ParseTree rootNode) {
+		FieldsContext insertFieldsContext=null;
+		//查找insert value子语句
+		for(int i=0;i<rootNode.getChildCount();i++){
+			ParseTree childContext = rootNode.getChild(i);
+			if (childContext instanceof InsertValuesClauseContext){
+				AssignmentValuesContext assignValueContext=null;
+				InsertValuesClauseContext insertValuesContext=(InsertValuesClauseContext)childContext;
+				for(int j=0;j<insertValuesContext.getChildCount();j++){
+					ParseTree valueContext = insertValuesContext.getChild(j);
+					if (valueContext instanceof FieldsContext){
+						insertFieldsContext=(FieldsContext)valueContext;
+					} else if (valueContext instanceof AssignmentValuesContext){
+						assignValueContext=(AssignmentValuesContext)valueContext;
+					}
+				}
+				if (insertFieldsContext==null){
+					throw new SQLParsingException("不允许插入语句不指定列:" + printSql(rootNode));
+				} else if (assignValueContext!=null){
+					ParseTree[] appendValues=getInsertFieldAndValue(userName);
+					insertFieldsContext.children.add(appendValues[0]);
+					insertFieldsContext.children.add(appendValues[1]);
+					assignValueContext.children.add(appendValues[2]);
+					assignValueContext.children.add(appendValues[3]);
+					
+				}
+				
+			} else if (childContext instanceof InsertSelectClauseContext){
+				InsertSelectClauseContext insertSelectContext=(InsertSelectClauseContext)childContext;
+				ProjectionsContext insertProjectionsContext=null;
+				for(int j=0;j<insertSelectContext.getChildCount();j++){
+					ParseTree valueContext = insertSelectContext.getChild(j);
+					if (valueContext instanceof FieldsContext){
+						insertFieldsContext=(FieldsContext)valueContext;
+					} else if (valueContext instanceof SelectContext){
+						insertProjectionsContext=findFirstClassType(valueContext,ProjectionsContext.class);
+						rewriteTableFactor(userName,valueContext);
+					}
+				}
+				if (insertFieldsContext==null){
+					throw new SQLParsingException("不允许插入语句不指定列:" + printSql(rootNode));
+				} else if (insertProjectionsContext!=null){
+					ParseTree[] appendValues=getInsertFieldAndSelectValue(userName);
+					insertFieldsContext.children.add(appendValues[0]);
+					insertFieldsContext.children.add(appendValues[1]);
+					insertProjectionsContext.children.add(appendValues[2]);
+					insertProjectionsContext.children.add(appendValues[3]);
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * 
+	 * @return 固定长度为4，前两个为字段，后两个为值
+	 */
+	private ParseTree[] getInsertFieldAndValue(String userName) {
+		ParseTree[] result=new ParseTree[4];
+		String insertSql="insert into t(a,tanentId) values('','" + userName +"')";
+		ParseASTNode parseASTNode = parserEngine.parse(insertSql, false);
+		ParseTree rootNode = parseASTNode.getRootNode();
+		InsertValuesClauseContext insertValuesContext=(InsertValuesClauseContext)rootNode.getChild(4);
+		FieldsContext fieldsContext=(FieldsContext)insertValuesContext.getChild(1);
+		AssignmentValuesContext valuesContext=(AssignmentValuesContext)insertValuesContext.getChild(4);
+		result[0]=fieldsContext.getChild(1);
+		result[1]=fieldsContext.getChild(2);
+		result[2]=valuesContext.getChild(2);
+		result[3]=valuesContext.getChild(3);
+		return result;
+	}
+
+	/**
+	 * 
+	 * @return 固定长度为4，前两个为字段，后两个为值
+	 */
+	private ParseTree[] getInsertFieldAndSelectValue(String userName) {
+		ParseTree[] result=new ParseTree[4];
+		String insertSql="insert into table1(name,tanent_id) select name,'" + userName + "' from table2";
+		ParseASTNode parseASTNode = parserEngine.parse(insertSql, false);
+		ParseTree rootNode = parseASTNode.getRootNode();
+		InsertSelectClauseContext insertSelectContext=(InsertSelectClauseContext)rootNode.getChild(4);
+		FieldsContext fieldsContext=(FieldsContext)insertSelectContext.getChild(1);
+		SelectContext selectContext=(SelectContext)insertSelectContext.getChild(3);
+		ProjectionsContext projectsContext=findFirstClassType(selectContext,ProjectionsContext.class);
+		result[0]=fieldsContext.getChild(1);
+		result[1]=fieldsContext.getChild(2);
+		result[2]=projectsContext.getChild(1);
+		result[3]=projectsContext.getChild(2);
+		return result;
+	}
+
+
+	public void rewriteUpdateContext(String userName,ParseTree rootNode) {
+		WhereClauseContext whereClauseContext=findWhereContext(rootNode);
+		if (whereClauseContext==null){
+			throw new SQLParsingException("更新必须包含条件语句," + printSql(rootNode));
+		}
+		SetAssignmentsClauseContext setAssignContext=findSetAssignContext(rootNode);
+		for(int i=0;i<setAssignContext.getChildCount();i++){
+			ParseTree childNode = setAssignContext.getChild(i);
+			if(childNode instanceof AssignmentContext){
+				if (!checkColumnNameValid(childNode)){
+					throw new SQLParsingException("更新语句不允许包含租户字段," + printSql(rootNode));
+				}
+			}
+		}
+		ExprContext expressContext=getConditionExpressionContext(userName,whereClauseContext.getChild(1));
+		whereClauseContext.children.set(1, expressContext);
+		rewriteTableFactor(userName,whereClauseContext);
+	}
+
+	/**
+	 * 更新语句的列名不能有租户字段
+	 * @param childNode
+	 */
+	private boolean checkColumnNameValid(ParseTree assignmentNode) {
+		for (int i = 0; i < assignmentNode.getChildCount(); i++) {
+			ParseTree childNode = assignmentNode.getChild(i);
+			if (childNode instanceof ColumnRefContext){
+				TerminalNodeImpl nameNode=(TerminalNodeImpl) childNode.getChild(0).getChild(0).getChild(0);
+				if ("tanent_id".equalsIgnoreCase(nameNode.getText())){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+
+	private SetAssignmentsClauseContext findSetAssignContext(ParseTree rootNode) {
+		for(int i=0;i<rootNode.getChildCount();i++){
+			ParseTree childContext=rootNode.getChild(i);
+			if(childContext instanceof SetAssignmentsClauseContext){
+				return (SetAssignmentsClauseContext) childContext;
+			}
+		}
+		return null;
+	}
+
+	public void rewriteDeleteContext(String userName,ParseTree rootNode) {
+		WhereClauseContext whereClauseContext=findWhereContext(rootNode);
+		if (whereClauseContext==null){
+			throw new SQLParsingException("删除必须包含条件语句," + printSql(rootNode));
+		}
+		ExprContext expressContext=getConditionExpressionContext(userName,whereClauseContext.getChild(1));
+		whereClauseContext.children.set(1, expressContext);
+		rewriteTableFactor(userName,whereClauseContext);
+	}
+
+	private ExprContext getConditionExpressionContext(String userName,ParseTree originParseTree){
+		String deleteSql="delete from table1 where  tanent_id='" + userName +"' and (name='b')";
+		ParseASTNode parseASTNode = parserEngine.parse(deleteSql, false);
+		ParseTree rootNode = parseASTNode.getRootNode();
+		WhereClauseContext whereContext=(WhereClauseContext) rootNode.getChild(3);
+		ExprContext result=(ExprContext) whereContext.getChild(1);
+		ExprContext subsituteContext=(ExprContext) result.getChild(2);
+		//找到()的父节点
+		SimpleExprContext simpleExprContext=(SimpleExprContext) subsituteContext.getChild(0).getChild(0).getChild(0).getChild(0);
+		simpleExprContext.children.set(1, originParseTree);
+		return result;
+	}
+	
+	private WhereClauseContext findWhereContext(ParseTree rootNode) {
+		for(int i=0;i<rootNode.getChildCount();i++){
+			ParseTree childContext=rootNode.getChild(i);
+			if(childContext instanceof WhereClauseContext){
+				return (WhereClauseContext) childContext;
+			}
+		}
+		return null;
+	}
+
+	public void rewriteTableFactor(String userName,ParseTree rootNode) {
+		int childCount=rootNode.getChildCount();
+		if (rootNode instanceof TableFactorContext) {
+			TableFactorContext factor=(TableFactorContext)rootNode;
+			int tableFactorCount= factor.getChildCount();
+			TableNameContext tableNameContext=null;
+			boolean hasAlias=false;
+			for(int i=0;i<tableFactorCount;i++){
+				ParseTree childContext = factor.getChild(i);
+				if (childContext instanceof TableNameContext){
+					tableNameContext=(TableNameContext)childContext;
+				} else if (childContext instanceof AliasContext){
+					hasAlias=true;
+				}
+			}
+			//如果没有别名那就使用原表名增加一个别名，如果有别名那就直接将表替换为子查询
+			if (tableNameContext!=null){
+				TerminalNodeImpl tableNode= getTableName(tableNameContext);
+				String tableName=tableNode.getText();
+				if (isTableShouldRewrite(tableName)){
+					SubqueryContext subqueryContext = findFirstClassType(createReplaceSubQuery(userName,tableName,hasAlias), SubqueryContext.class);
+					if (hasAlias){
+						factor.children.set(0, subqueryContext);
+					} 
+					else {
+						factor.children.remove(0);
+						factor.children.add(subqueryContext.getParent());
+					}
+				}
+			}
+
+		}
+		else {
+			for(int i=0;i<childCount;i++) {
+				ParseTree child = rootNode.getChild(i);
+				if (child!=null) {
+					rewriteTableFactor(userName,child);
+				}
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param tableNode
+	 * @param hasAlias 原始表有别名的话，替换的子查询不需要别名
+	 * @return
+	 */
+	private ParseTree createReplaceSubQuery(String userName,String tableName,boolean hasAlias) {
+		String subQuery="select * from (select * from " + tableName +" where tanentId='" + userName +"')" + (hasAlias?"":" " + tableName);
+		ParseASTNode parseASTNode = parserEngine.parse(subQuery, false);
+		ParseTree rootNode = parseASTNode.getRootNode();
+		printStructure(rootNode, 0);
+		return rootNode;
+	}
+
+	private TerminalNodeImpl getTableName(TableNameContext tableNameContext) {
+		ParseTree tableNode = tableNameContext.getChild(0).getChild(0).getChild(0);
+		return (TerminalNodeImpl)tableNode;
+	}
+
+	public static String rewriteSql(ConnectionSession connectionSession,String sql){
+		PostgresViewRewriter sqlWriter = getInstance();
+		String userName=connectionSession.getConnectionContext().getGrantee().getUsername();
+		if(userName!=null){
+			String newSql= sqlWriter.rewriteSql(userName,sql);
+			return newSql;
+		}
+		throw new SQLParsingException("异常的数据库账号信息:" + sql);
+	}
+
+}
--- src/test/java/com/chinatelecom/udp/component/shardingsphere/ShardingsphereSqlViewRewriteTest.java
+++ src/test/java/com/chinatelecom/udp/component/shardingsphere/ShardingsphereSqlViewRewriteTest.java
@@ -19,7 +19,7 @@ import org.junit.Test;
 public class ShardingsphereSqlViewRewriteTest {
 
 	@Test
-	public void testModifyTable() {
+	public void testMySqlRewriter() {
 		String result;
 		DatabaseType databaseType = TypedSPILoader.getService(DatabaseType.class, "MySQL");
 		SQLParserEngine parserEngine = new SQLParserEngine(databaseType, new CacheOption(2000, 65535L));
@@ -127,5 +127,120 @@ public class ShardingsphereSqlViewRewriteTest {
 						") table2;").equals(result));
 		
 	}
+
+}
+
+
+
+
+@Test
+public void testPostgresRewriter() {
+	String result;
+	DatabaseType databaseType = TypedSPILoader.getService(DatabaseType.class, "PostgreSQL");
+	SQLParserEngine parserEngine = new SQLParserEngine(databaseType, new CacheOption(2000, 65535L));
+	PostgresViewRewriter rewriter=new PostgresViewRewriter(parserEngine);
+	rewriter.analyseSql("update table1 set name='b',name='c' where name='b' and name in (select name from table2)");
+	result=rewriter.rewriteSql("user","update table1 set name='b',name='c' where name='b' and name in (select name from table2)");
+	assertTrue(("UPDATE  table1 SET name = 'b',\n" + //
+					"\tname = 'c' WHERE \n" + //
+					"\ttanent_id = 'user'\n" + //
+					"\tand (name = 'b'\n" + //
+					"\tand name IN \n" + //
+					"\t(\n" + //
+					"\t\tSELECT name \n" + //
+					"\t\tFROM \n" + //
+					"\t\t(\n" + //
+					"\t\t\tSELECT * \n" + //
+					"\t\t\tFROM view1\n" + //
+					"\t\t\tWHERE \n" + //
+					"\t\t\t\ttanentId = 'user'\n" + //
+					"\t\t) table2\n" + //
+					"\t))").equals(result));
+
+	rewriter.analyseSql("select * from (select * from view1) t where name='bbb'");
+	rewriter.analyseSql("select * from (select * from view1)");
+	
+	result=rewriter.rewriteSql("user","select * from table1 where name='bbb'");
+	assertTrue(("SELECT * \n" + //
+					"FROM \n" + //
+					"(\n" + //
+					"\tSELECT * \n" + //
+					"\tFROM view1\n" + //
+					"\tWHERE \n" + //
+					"\t\ttanentId = 'user'\n" + //
+					") table1\n" + //
+					"WHERE \n" + //
+					"\tname = 'bbb';").equals(result));
+
+	result=rewriter.rewriteSql("user","select * from table1 t where name='bbb'");
+	assertTrue(("SELECT * \n" + //
+					"FROM \n" + //
+					"(\n" + //
+					"\tSELECT * \n" + //
+					"\tFROM view1\n" + //
+					"\tWHERE \n" + //
+					"\t\ttanentId = 'user'\n" + //
+					") t\n" + //
+					"WHERE \n" + //
+					"\tname = 'bbb';").equals(result));
+	
+	rewriter.analyseSql("insert into table1 values('a','v')");
+	try{
+		rewriter.rewriteSql("user","insert into table1 values('a','v')");
+		assertTrue("应抛出语句异常错误", false);
+	} catch (SQLParsingException e){
+		assertTrue(e.getMessage().indexOf("不允许插入语句不指定列")!=-1);
+	}
+	rewriter.analyseSql("insert into table1(name) values('a')");
+	rewriter.analyseSql("insert into table1(name,id) values('a','b')");
+	result= rewriter.rewriteSql("user","insert into table1(name) values('a')");
+	assertTrue(("INSERT  INTO table1 (name , tanentId)\n" + //
+					"VALUES\n" + //
+					"\t('a', 'user')").equals(result));
+	//实际执行是会报错，提示存在多个列
+	rewriter.rewriteSql("user","insert into table1(name,tanentId) values('a',?)");
+
+	rewriter.analyseSql("delete from table1 where name='b'");
+	rewriter.analyseSql("delete from table1 where  tanent_id='user' and (name='b')");
+
+
+	rewriter.analyseSql("delete from table1 where name='b' or name='c'");
+	rewriter.analyseSql("delete from table1 where tanent_id='user' and (name='b' or name='c')");
+
+	result=rewriter.rewriteSql("user","delete from table1 where name='b'");
+	assertTrue(("DELETE  FROM table1 WHERE \n" + //
+					"\ttanent_id = 'user'\n" + //
+					"\tand (name = 'b')").equals(result));
+	result=rewriter.rewriteSql("user","delete from table1 where name='b' or name='c'");
+	assertTrue(("DELETE  FROM table1 WHERE \n" + //
+					"\ttanent_id = 'user'\n" + //
+					"\tand (name = 'b'\n" + //
+					"\tor name = 'c')").equals(result));
+
+
+	rewriter.analyseSql("update table1 set name='b',name='c' where name='b'");
+	rewriter.analyseSql("update table1 set name='b' ,name='c' where tanent_id='user' and (name='b')");
+	
+	result=rewriter.rewriteSql("user","update table1 set name='b',name='c' where name='b'");
+
+	assertTrue(("UPDATE  table1 SET name = 'b',\n" + //
+					"\tname = 'c' WHERE \n" + //
+					"\ttanent_id = 'user'\n" + //
+					"\tand (name = 'b')").equals(result));
+
+	rewriter.analyseSql("insert into table1(name) select name from table2");
+	rewriter.analyseSql("insert into table1(name,tanent_id) select name,'user' from table2");
+	result=rewriter.rewriteSql("user","insert into table1(name) select name from table2");
+	assertTrue(("INSERT  INTO table1 (name , tanent_id) \n" + //
+					"SELECT name , 'user' \n" + //
+					"FROM \n" + //
+					"(\n" + //
+					"\tSELECT * \n" + //
+					"\tFROM view1\n" + //
+					"\tWHERE \n" + //
+					"\t\ttanentId = 'user'\n" + //
+					") table2;").equals(result));
+	
+}
 
 }
